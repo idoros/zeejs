f7ffe19d84c5ce380ed6a4cf8a8dbef99b1b8226
Ido Rosenthal <idoros@gmail.com>
2020-05-03 16:14:46 +0300

    feat(react): stop overlay bind on layer destory

--- packages/react/src/root.tsx
+++ packages/react/src/root.tsx
@@ -10,8 +10,10 @@ import React, {
     useCallback,
 } from 'react';
 
+const overlayBind = Symbol(`overlay-bind`);
 interface LayerExtended {
     element: HTMLElement;
+    [overlayBind]: ReturnType<typeof bindOverlay>;
 }
 interface LayerSettings {
     relativeTo?: `window` | HTMLElement;
@@ -41,35 +43,37 @@ export const Root = ({ className, style, children }: RootProps) => {
         }
     }, []);
 
-    const layer = useMemo(
-        () =>
-            createLayer({
-                extendLayer: {
-                    element: null as unknown as HTMLElement,
-                } as LayerExtended,
-                defaultSettings: {
-                    relativeTo: `window`,
-                } as LayerSettings,
-                onChange() {
-                    updateLayers();
-                },
-                init(layer, settings) {
-                    layer.element = document.createElement(`div`); // ToDo: test that each layer has a unique element
-                    if (layer.parentLayer) {
-                        if (settings.relativeTo === 'window') {
-                            layer.element.setAttribute(
-                                `style`,
-                                `position: fixed;top: 0;left: 0;right: 0;bottom: 0;`
-                            );
-                        } else if (settings.relativeTo instanceof HTMLElement) {
-                            // ToDo: handle bind stop when overlay is removed
-                            bindOverlay(settings.relativeTo, layer.element);
-                        }
+    const layer = useMemo(() => {
+        return createLayer({
+            extendLayer: {
+                element: (null as unknown) as HTMLElement,
+            } as LayerExtended,
+            defaultSettings: {
+                relativeTo: `window`,
+            } as LayerSettings,
+            onChange() {
+                updateLayers();
+            },
+            init(layer, settings) {
+                layer.element = document.createElement(`div`); // ToDo: test that each layer has a unique element
+                if (layer.parentLayer) {
+                    if (settings.relativeTo === 'window') {
+                        layer.element.setAttribute(
+                            `style`,
+                            `position: fixed;top: 0;left: 0;right: 0;bottom: 0;`
+                        );
+                    } else if (settings.relativeTo instanceof HTMLElement) {
+                        layer[overlayBind] = bindOverlay(settings.relativeTo, layer.element);
                     }
-                },
-            }),
-        []
-    );
+                }
+            },
+            destroy(layer) {
+                if (layer[overlayBind]) {
+                    layer[overlayBind].stop(); // not tested because its a side effect:/
+                }
+            },
+        });
+    }, []);
 
     useEffect(() => {
         layer.element = rootRef.current!.firstElementChild! as HTMLElement;
