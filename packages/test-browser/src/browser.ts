019c2b41acfd384e34246937de93cb89e8d58319
Ido Rosenthal <idoros@gmail.com>
2021-11-23 12:17:29 +0200

    infra: suuport A11y tree in tests

--- packages/test-browser/src/browser.ts
+++ packages/test-browser/src/browser.ts
@@ -1,6 +1,8 @@
 export * as imageSnapshotShared from './image-snapshot/shared';
 export * as interactionApiShared from './interaction-api/shared';
+export * as A11yShared from './accessibility/shared';
 export * as serverFixturesShared from './server-fixtures/shared';
 export type { InteractionApi } from './interaction-api/shared';
 export type { ServerFixturesOptions, ServerFixtureHookMessage } from './server-fixtures/shared';
 export type { ImageSnapshotOptions, ImageSnapshotHookMessage } from './image-snapshot/shared';
+export type { A11ySnapshot, A11yNode } from './accessibility/shared';
